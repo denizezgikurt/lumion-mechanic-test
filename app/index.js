f9c9168c13803dfb9a287da4ef8bf89f78ba993a
denizezgikurt <deniz.kurt@dentsucreative.amsterdam>
2024-01-11 11:39:32 +0100

    new

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import CaptionGenerator from "./CaptionGenerator"; 
+
 
 import "../assets/fonts.css";
 import * as css from "./index.module.css";
@@ -10,12 +12,12 @@ const Head = () => {
       htmlAttributes={{
         lang: "en"
       }}
-      defaultTitle="AIxDesign Toolkit"
+      defaultTitle="Lumion Toolkit"
     >
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <title>AIxDesign Toolkit</title>
-      <meta name="description" content="AIxDesign Toolkit" />
+      <title>Lumion Toolkit</title>
+      <meta name="description" content="Lumion Toolkit" />
       <link rel="icon" href="static/favicon.svg" type="image/svg+xml" />
     </Helmet>
   );
@@ -25,7 +27,8 @@ export const SideBar = props => {
   return (
     <aside className={css.root}>
       <Head />
-      <h1>AIxDesign Toolkit Instagram Post</h1>
+      <CaptionGenerator />
+      <h1>Lumion Toolkit </h1>
       {props.children}
     </aside>
   );
